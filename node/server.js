2006d7edba472ff2c92ed2f33b44eb534e79207a
AgostinhoMayoral <agostinhomayoral@gmail.com>
2025-01-30 20:16:00 -0300

    fix server

--- node/server.js
+++ node/server.js
@@ -1,26 +1,41 @@
-const express = require("express");
-const mysql = require("mysql2");
-
+const mysql = require('mysql2');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao MySQL:", err);
-    return;
-  }
-  console.log("Conectado ao MySQL");
-  db.query("CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))");
-});
+// Função para conectar ao banco de dados com lógica de reconexão
+function connectDB() {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
 
-app.get("/", (req, res) => {
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      setTimeout(connectDB, 5000); // Tenta novamente em 5 segundos
+    } else {
+      console.log('Connected to the database2!');
+    }
+  });
+
+  // Limpeza da conexão em caso de erro fatal
+  db.on('error', (err) => {
+    console.error('Database connection error:', err);
+    if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+      db.destroy(); // Limpa a conexão em caso de erro fatal
+      setTimeout(connectDB, 5000); // Tenta reconectar após 5 segundos
+    }
+  });
+}
+
+connectDB();
+console.log('----------------------------------------------------------------');
+app.get('/', (req, res) => {
   const name = `Usuário ${Math.floor(Math.random() * 1000)}`;
   db.query("INSERT INTO people (name) VALUES (?)", [name], (err) => {
     if (err) {
@@ -43,5 +58,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
