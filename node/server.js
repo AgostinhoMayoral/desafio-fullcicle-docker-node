243e1764564dd64b7a8df95fdb9375435a99bc07
AgostinhoMayoral <agostinhomayoral@gmail.com>
2025-01-30 21:44:59 -0300

    fix connection db

--- node/server.js
+++ node/server.js
@@ -1,46 +1,31 @@
-const mysql = require('mysql2');
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
 const port = 3000;
 
-let db;
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-// Função para conectar ao banco de dados com lógica de reconexão
-function connectDB() {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
+db.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+    return;
+  }
+  console.log("Conectado ao MySQL");
+  db.query("CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))");
+});
 
-  db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      setTimeout(connectDB, 5000); // Tenta novamente em 5 segundos
-    } else {
-      console.log('Connected to the database2!');
-    }
-  });
-
-  // Limpeza da conexão em caso de erro fatal
-  db.on('error', (err) => {
-    console.error('Database connection error:', err);
-    if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
-      db.destroy(); // Limpa a conexão em caso de erro fatal
-      setTimeout(connectDB, 5000); // Tenta reconectar após 5 segundos
-    }
-  });
-}
-
-connectDB();
-console.log('----------------------------------------------------------------');
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const name = `Usuário ${Math.floor(Math.random() * 1000)}`;
   db.query("INSERT INTO people (name) VALUES (?)", [name], (err) => {
     if (err) {
-      console.error("Erro ao inserir nome:", err);
-      res.status(500).send("Erro ao inserir nome");
+      //console.error("Erro ao inserir nome:", err);
+      res.status(500).send(`Erro ao inserir nome `);
       return;
     }
 
@@ -58,5 +43,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
